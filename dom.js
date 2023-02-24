5c9442ff9613d90bc237f79c63a3551520d04c5c
jitenderkumar15 <jitender5353@gmail.com>
2023-02-24 17:00:10 +0530

    secondfile

--- dom.js
+++ dom.js
@@ -1,5 +1,9 @@
+// this code will work by adding a id(addtm) in ADD ITEAMS//
+
+
 let headtext=document.getElementById('addtm');
 headtext.textContent='hello';
 headtext.style.fontWeight='bold';
 
 headtext.style.border='solid 3px #000';
+
