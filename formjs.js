078b8e3e2a5593cc8ddf3e16dfbcb99693223af1
jitenderkumar15 <jitender5353@gmail.com>
2023-02-24 11:13:51 +0530

    demo

---
+++ formjs.js
@@ -0,0 +1,42 @@
+const btn=document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background='#ccc';
+    document.querySelector('body').style.background='#ccc';
+});
+
+btn.addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background='red';
+    document.querySelector('body').style.background='yellow';
+    
+
+});
+
+btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background='orange';
+    document.querySelector('body').style.background='black';
+});
+
+
+
+const myForm= document.querySelector('#my-form');
+const InName=document.querySelector('#exampleName');
+const Inemail=document.querySelector('#exampleInputEmail1');
+
+myForm.addEventListener('submit', onSubmit); 
+
+function onSubmit(e){
+    e.preventDefault();
+
+    console.log(InName.value);
+
+    // if(InName.value ==='' || Inemail.value ===''){
+    //     alert('Please enter the value');
+    // }
+    // else{
+    //     console.log('success');
+    // }
+}
