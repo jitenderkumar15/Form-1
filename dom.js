ad0e4fd9fc144ab611a821e76cfe70388ae8989e
jitenderkumar15 <98521193+jitenderkumar15@users.noreply.github.com>
2023-02-25 17:26:56 +0530

    task 6

--- dom.js
+++ dom.js
@@ -1,23 +1,37 @@
 // this code will work by adding a id(addtm) in ADD ITEAMS//
 
 
-let headtext=document.getElementById('addtm');
-headtext.textContent='hello';
-headtext.style.fontWeight='bold';
+// let headtext=document.getElementById('addtm');
+// headtext.textContent='hello';
+// headtext.style.fontWeight='bold';
 
-headtext.style.border='solid 3px #000';
+// headtext.style.border='solid 3px #000';
 
-var iteams=document.getElementsByClassName('list-group-item');
-console.log(iteams);
-iteams[2].style.background='green';
-iteams[3].style.fontWeight='bold';
-iteams[2].style.fontWeight='bold';
-iteams[1].style.fontWeight='bold';
-iteams[0].style.fontWeight='bold';
+// var iteams=document.getElementsByClassName('list-group-item');
+// console.log(iteams);
+// iteams[1].style.background='green';
+// iteams[2].style.color='white';
+// iteams[3].style.fontWeight='bold';
+// // iteams[2].style.fontWeight='bold';
+// // iteams[1].style.fontWeight='bold';
+// iteams[0].style.fontWeight='bold';
+
+// var iteam5=document.getElementsByClassName('list2');
+// iteam5[0].style.background='yellow';
+
+// var iteam51=document.getElementsByTagName('li');
+// iteam51[4].style.border='3px solid';
+
+var odd = document.querySelectorAll('li');
+odd[1].style.color='green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
 
 
-var iteam5=document.getElementsByClassName('list2');
-iteam5[0].style.background='yellow';
 
-var iteam51=document.getElementsByTagName('li');
-iteam51[4].style.border='3px solid';
+
+
