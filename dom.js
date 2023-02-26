b29a969a269fc5d2576446ab80627cf0e50e6a70
jitenderkumar15 <98521193+jitenderkumar15@users.noreply.github.com>
2023-02-26 15:22:24 +0530

    task--7

--- dom.js
+++ dom.js
@@ -33,7 +33,105 @@
 
 
 task----7
+// console.log('i love git')
 
+// dom transver
+var itemList = document.querySelector('#items');
+
+// parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.background='grey';
+
+// createchild
+// createelement
+// setAttribute
+// createtesxtnode
+// appendchild
+
+//lastElementChild lastChild
+
+itemList.lastElementChild.textContent='olla';
+console.log(itemList.lastElementChild);
+console.log(itemList.lastChild)
+itemList.lastElementChild.style.background='pink';
+
+// firstchild firstelementchild
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+
+itemList.firstElementChild.textContent='hello';
+itemList.firstElementChild.style.background='blue';
+
+//nextSibling
+var form1 = document.querySelector('form');
+console.log(form1.nextSibling);
+//nextElementSibling
+console.log(form1.nextElementSibling);
+form1.nextElementSibling.style.color='red';
+
+//previousSibling
+console.log(itemList.previousSibling.previousSibling.previousSibling);
+// previousElementSibling
+console.log(itemList.previousElementSibling.previousElementSibling.previousElementSibling);
+
+itemList.previousElementSibling.previousElementSibling.previousElementSibling.style.color='yellow';
+
+// createElement
+
+//create div
+var newDiv=document.createElement('div');
+
+//add class
+newDiv.className='hello';
+
+//add id
+newDiv.id='hello1';
+
+//add attribute
+newDiv.setAttribute('title','hello div');
+
+//create text node
+var newDivText=document.createTextNode('Hello World');
+
+//add text to div
+newDiv.appendChild(newDivText);
+
+//create child- adding HEllo before iteam Listner
+
+var container1=document.querySelector('header .container');
+console.log(container1);
+
+var h1=document.querySelector('header h1');
+
+container1.insertBefore(newDiv,h1);
+
+
+// createElement
+
+//create li
+var newDiv2=document.createElement('li');
+
+//add class
+newDiv2.className='list-group-item';
+
+//add id
+newDiv2.id='list-item';
+
+//add attribute
+newDiv2.setAttribute('title','hello li');
+
+//create text node
+var newDivText2=document.createTextNode('Hello World');
+
+//add text to li
+newDiv2.appendChild(newDivText2);
+
+
+var parentnode=document.querySelector('#items');
+
+var nextnode=document.querySelector('.list-group-item');
+
+parentnode.insertBefore(newDiv2,nextnode);
 
 
 
